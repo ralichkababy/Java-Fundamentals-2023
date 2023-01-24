cc8595042861fd760881a99ea5c1748b0d4ae518
Ralitsa Hristova <123333972+ralichkababy@users.noreply.github.com>
2023-01-25 00:52:23 +0200

    Create RageExpense.java

---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/RageExpense.java
@@ -0,0 +1,27 @@
+package BasicSyntaxExercise;
+
+import java.util.Scanner;
+
+public class RageExpenses {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int lostGames = Integer.parseInt(scanner.nextLine());
+        double headsetPrice = Double.parseDouble(scanner.nextLine());
+        double mousePrice = Double.parseDouble(scanner.nextLine());
+        double keyboardPrice = Double.parseDouble(scanner.nextLine());
+        double displayPrice = Double.parseDouble(scanner.nextLine());
+
+        int countHeadset = lostGames / 2; 
+        int countMouse = lostGames / 3; 
+        int countKeyboards = lostGames / 6; 
+        int countDisplays = lostGames / 12; 
+
+        double finalSum = (countHeadset * headsetPrice)
+                + (countMouse * mousePrice)
+                + (countKeyboards * keyboardPrice)
+                + (countDisplays * displayPrice);
+
+        System.out.printf("Rage expenses: %.2f lv.", finalSum);
+    }
+}
