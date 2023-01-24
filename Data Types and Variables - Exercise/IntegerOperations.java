94bf47562557a10d5d75e902ee104e03722a82a4
Ralitsa Hristova <123333972+ralichkababy@users.noreply.github.com>
2023-01-24 15:51:13 +0200

    Create IntegerOperations.java

---
+++ Data Types and Variables - Exercise/IntegerOperations.java
@@ -0,0 +1,18 @@
+package DataTypesExercise;
+
+import java.util.Scanner;
+
+public class IntegerOperations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int one = Integer.parseInt(scanner.nextLine());
+        int two = Integer.parseInt(scanner.nextLine());
+        int three = Integer.parseInt(scanner.nextLine());
+        int four = Integer.parseInt(scanner.nextLine());
+
+        int sum = ((one+two)/three)*four;
+
+        System.out.println(sum);
+    }
+}
