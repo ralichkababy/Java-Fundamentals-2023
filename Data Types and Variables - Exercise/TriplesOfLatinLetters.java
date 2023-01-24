acb0b88fc56f9fd13996c6cf022d822e059c01ea
Ralitsa Hristova <123333972+ralichkababy@users.noreply.github.com>
2023-01-24 21:57:01 +0200

    Create TriplesOfLatinLetters.java

---
+++ Data Types and Variables - Exercise/TriplesOfLatinLetters.java
@@ -0,0 +1,25 @@
+package DataTypesExercise;
+
+import java.util.Scanner;
+
+public class TriplesOfLatinLetters {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            char firstLetter = (char) ('a' + i);
+
+            for (int j = 0; j < n; j++) {
+                char secondLetter = (char) ('a' + j);
+
+                for (int k = 0; k < n; k++) {
+                    char thirdLetter = (char) ('a' + k);
+                    
+                    System.out.printf("%c%c%c%n", firstLetter, secondLetter, thirdLetter);
+                }
+            }
+    }
+}
+}
